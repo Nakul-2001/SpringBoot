c39281edad63eed16b44da27567cd9a96de1d3df
Nakul Sharma <nakul.sharma@astrotalk.com>
2024-11-09 14:24:36 +0530

    firstClassAdded

---
+++ src/main/java/com/sharma/nakul/MyFirstClass.java
@@ -0,0 +1,9 @@
+package com.sharma.nakul;
+
+import org.springframework.context.annotation.Bean;
+public class MyFirstClass {
+
+    public void sayHello() {
+        System.out.println("Hello");
+    }
+}
--- src/main/java/com/sharma/nakul/NakulApplication.java
+++ src/main/java/com/sharma/nakul/NakulApplication.java
@@ -1,5 +1,6 @@
 package com.sharma.nakul;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,8 @@ public class NakulApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NakulApplication.class, args);
+		MyFirstClass myFirstClass = new MyFirstClass();
+		myFirstClass.sayHello();
 	}
 
 }
